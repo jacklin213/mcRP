79e44601189f11d9b5700d7635036c1a654465d0
jack lin <jacklin213@gmail.com>
2012-08-18 19:02:08 +1200

    mcRPListener changed to DamageListener

---
+++ src/me/jacklin213/mcrp/DamageListener.java
@@ -0,0 +1,34 @@
+package me.jacklin213.mcrp;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.potion.*;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class DamageListener implements Listener {
+	public static mcRP plugin;
+
+	  public DamageListener(mcRP instance)
+	  {
+	    plugin = instance;
+	  }
+	
+	@EventHandler(priority = EventPriority.HIGH)
+	public void OnEntityDamageByEntity(Entity damager, Entity damagee,
+			EntityDamageEvent.DamageCause cause, int damage) {
+		Player d = (Player) damagee;
+		if (d.getHealth() <= 5) {
+			d.sendMessage(ChatColor.YELLOW + "You are now bleeding");
+			d.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 200, 1));
+			d.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200,
+					1));
+			d.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 1));
+		}
+
+	}
+
+}
