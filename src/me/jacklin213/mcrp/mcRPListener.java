70f79508b19583b631a18a797f0bd3e4b4335b7b
jack lin <jacklin213@gmail.com>
2012-08-18 15:14:55 +1200

    Listener class

---
+++ src/me/jacklin213/mcrp/mcRPListener.java
@@ -0,0 +1,15 @@
+package me.jacklin213.mcrp;
+
+public class mcRPListener {
+	public static mcRP plugin;
+
+	  public mcRPListener(mcRP instance)
+	  {
+	    plugin = instance;
+	  }
+	  
+	  
+	  
+	  
+	  //end of listener
+}
