4578263276373d4922bf75f5426663aed2e335a2
jack lin <jacklin213@gmail.com>
2012-08-18 18:37:27 +1200

    Updated Main class

--- src/me/jacklin213/mcrp/mcRP.java
+++ src/me/jacklin213/mcrp/mcRP.java
@@ -6,31 +6,31 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class mcRP extends JavaPlugin {
 	public static mcRP plugin;
 	PluginDescriptionFile pdfFile;
-	public final mcRPListener Listener = new mcRPListener(this);
+	public final DamageListener dl = new DamageListener(this);
 	public final Logger log = Logger.getLogger("Minecraft");
 	public static Permission Permissions = null;
-	
-	public void onEnable(){
+
+	public void onEnable() {
 		this.pdfFile = getDescription();
-		this.log.info(this.pdfFile.getName() + this.pdfFile.getVersion() + " By jacklin213 & TickleNinja is now enabled!.");
-		
+		this.log.info(this.pdfFile.getName() + this.pdfFile.getVersion()
+				+ " By jacklin213 & TickleNinja is now enabled!.");
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this.dl, this);
 	}
-	
-	public void onDisable(){
+
+	public void onDisable() {
 		this.pdfFile = getDescription();
 		this.log.info(this.pdfFile.getName() + " is now disabled.");
 	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String [] args){
+
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String commandLabel, String[] args) {
 		return false;
-	}
-	
-	
-	
-	//end of main class
+	} // end of main class
 }
