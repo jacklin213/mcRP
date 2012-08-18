6ff0855b31efc58e2861cf6dbac63299edb7eff1
jack lin <jacklin213@gmail.com>
2012-08-18 15:14:47 +1200

    Main class

---
+++ src/me/jacklin213/mcrp/mcRP.java
@@ -0,0 +1,36 @@
+package me.jacklin213.mcrp;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class mcRP extends JavaPlugin {
+	public static mcRP plugin;
+	PluginDescriptionFile pdfFile;
+	public final mcRPListener Listener = new mcRPListener(this);
+	public final Logger log = Logger.getLogger("Minecraft");
+	public static Permission Permissions = null;
+	
+	public void onEnable(){
+		this.pdfFile = getDescription();
+		this.log.info(this.pdfFile.getName() + this.pdfFile.getVersion() + " By jacklin213 & TickleNinja is now enabled!.");
+		
+	}
+	
+	public void onDisable(){
+		this.pdfFile = getDescription();
+		this.log.info(this.pdfFile.getName() + " is now disabled.");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String [] args){
+		return false;
+	}
+	
+	
+	
+	//end of main class
+}
