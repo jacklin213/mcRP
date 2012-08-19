0fd02df05c3f5454cf00b92baa83b98662ecf518
PineAbe <kennethmedida@hotmail.com>
2012-08-19 16:34:37 +1000

    Fixed Event!

--- src/me/jacklin213/mcrp/DamageListener.java
+++ src/me/jacklin213/mcrp/DamageListener.java
@@ -1,13 +1,12 @@
 package me.jacklin213.mcrp;
 
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.potion.*;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class DamageListener implements Listener {
 	public static mcRP plugin;
@@ -16,8 +15,8 @@ public class DamageListener implements Listener {
 		
 	}
 	@EventHandler(priority = EventPriority.HIGH)
-	public void OnEntityDamageByEntity(Entity damager, Entity damagee, EntityDamageEvent.DamageCause cause, int damage) {
-		Player d = (Player) damagee;
+	public void OnEntityDamageByEntity(EntityDamageByEntityEvent event) {
+		Player d = (Player) event.getEntity();
 		if (d.getHealth() <= 5) {
 			d.sendMessage(ChatColor.YELLOW + "You are now bleeding");
 			d.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 200, 1));
