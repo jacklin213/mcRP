4c4d8c9e105cc324c2302cb3a38987e058804a7c
PineAbe <kennethmedida@hotmail.com>
2012-08-19 15:46:05 +1000

    Testing Commands

--- src/me/jacklin213/mcrp/mcRP.java
+++ src/me/jacklin213/mcrp/mcRP.java
@@ -4,10 +4,13 @@ import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class mcRP extends JavaPlugin {
 	public static mcRP plugin;
@@ -17,11 +20,12 @@ public class mcRP extends JavaPlugin {
 	public static Permission Permissions = null;
 
 	public void onEnable() {
+		
 		this.pdfFile = getDescription();
-		this.log.info(this.pdfFile.getName() + this.pdfFile.getVersion()
-				+ " By jacklin213 & TickleNinja is now enabled!.");
+		this.log.info(this.pdfFile.getName() + this.pdfFile.getVersion() + " By jacklin213 & TickleNinja is now enabled!.");
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(this.dl, this);
+		
 	}
 
 	public void onDisable() {
@@ -29,8 +33,19 @@ public class mcRP extends JavaPlugin {
 		this.log.info(this.pdfFile.getName() + " is now disabled.");
 	}
 
-	public boolean onCommand(CommandSender sender, Command cmd,
-			String commandLabel, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+	    if(commandLabel.equalsIgnoreCase("test")){
+	    	Player p = (Player) sender;
+	    	p.setHealth(6);
+	    	p.sendMessage("works");
+	    }
+	    if(commandLabel.equalsIgnoreCase("test2")){
+	    	Player p = (Player) sender;
+	    	p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 200, 1));
+	    	p.sendMessage("works");
+	    }
 		return false;
+		
 	} // end of main class
+	
 }
