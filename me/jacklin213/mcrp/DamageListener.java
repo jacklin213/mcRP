4f501a8e11ea4b1ce5e15be3279b3e5cd30c6c4f
Kenneth Medida <kennethmedida@hotmail.com>
2012-08-21 20:45:05 +1000

    Re-Synced everything + Manifest.mf BugFix

---
+++ me/jacklin213/mcrp/DamageListener.java
@@ -0,0 +1,32 @@
+package me.jacklin213.mcrp;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.potion.*;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class DamageListener implements Listener {
+	public static mcRP plugin;
+
+	public DamageListener(mcRP instance) {
+		plugin = instance;
+	}
+
+	@EventHandler(priority = EventPriority.HIGH)
+	public void OnEntityDamageByEntity(EntityDamageByEntityEvent event) {
+		Player d = (Player) event.getEntity();
+		if (d.getHealth() <= 5) {
+			d.sendMessage(ChatColor.YELLOW + "You are" + ChatColor.RED
+					+ "bleeding");
+			d.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 200, 1));
+			d.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200,
+					1));
+			d.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 1));
+		}
+
+	}
+}
+
