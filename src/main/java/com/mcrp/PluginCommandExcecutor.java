e5f9ec8251d4b72cb2c14387ec3a14a97f8300b7
Keeley Hoek <escortkeel@live.com>
2012-08-31 18:35:24 +1000

    Cleaned up CommandExcecutor

--- src/main/java/com/mcrp/PluginCommandExcecutor.java
+++ src/main/java/com/mcrp/PluginCommandExcecutor.java
@@ -99,35 +99,29 @@ public class PluginCommandExcecutor implements CommandExecutor {
             } else if (args.length > 1) {
                 if (args[0].equalsIgnoreCase("set")) {
                     if (args.length < 1) {
-                        sender.sendMessage(ChatColor.RED
-                                + "Not a valid command !");
+                        sender.sendMessage(ChatColor.RED + "Not a valid command!");
+
                         return true;
-                    }
-
-                    if (args[1].equalsIgnoreCase("welcomemessage")
-                            || args[1].equalsIgnoreCase("wm")) {
-                        if (!(sender.hasPermission("mcRP.setwelcomemessage"))) {
-                            sender.sendMessage(Plugin.getChatName() + " You don't have the permission to do this");
-                            return false;
-                        } else {
-                            StringBuilder sb = new StringBuilder();
-                            for (int i = 0; i < args.length; i++) {
-                                if (i != 0) {
-                                    sb.append(' ');
+                    } else {
+                        if (args[1].equalsIgnoreCase("welcomemessage") || args[1].equalsIgnoreCase("wm")) {
+                            if (!(sender.hasPermission("mcRP.setwelcomemessage"))) {
+                                sender.sendMessage(Plugin.getChatName() + " You don't have the permission to do this");
+                            } else {
+                                StringBuilder sb = new StringBuilder();
+                                for (int i = 0; i < args.length; i++) {
+                                    if (i != 0) {
+                                        sb.append(' ');
+                                    }
+                                    sb.append(args);
                                 }
-                                sb.append(args);
+
+                                sender.sendMessage(Plugin.getChatName() + " The message has been changed to: " + sb.toString().replaceAll("(&([a-f0-9]))", "\u00A7$2"));
+                                plugin.getConfig().set("WelcomeMessage", sb.toString().replaceAll("(&([a-f0-9]))", "\u00A7$2"));
+                                plugin.saveConfig();
                             }
-
-                            sender.sendMessage(Plugin.getChatName() + " The message has been changed to: " + sb.toString().replaceAll("(&([a-f0-9]))", "\u00A7$2"));
-                            plugin.getConfig().set("WelcomeMessage", sb.toString().replaceAll("(&([a-f0-9]))", "\u00A7$2"));
-                            plugin.saveConfig();
-
-                            return true;
                         }
                     }
                 }
-
-                return false;
             }
 
             sender.sendMessage(ChatColor.DARK_GREEN + "+-----------------------------------+");
@@ -135,32 +129,37 @@ public class PluginCommandExcecutor implements CommandExecutor {
             sender.sendMessage(ChatColor.GREEN + "By jacklin213, TickleNinja");
             sender.sendMessage(ChatColor.GOLD + "Version: " + plugin.getDescription().getVersion());
             sender.sendMessage(ChatColor.DARK_GREEN + "+-----------------------------------+");
-
-            return true;
         } else if (alias.equalsIgnoreCase("skills")) {
             if (args.length == 1) {
                 if (sender instanceof Player) {
                     Player p = (Player) sender;
                     if (args[0].equalsIgnoreCase("superspeed")) {
                         plugin.getSkillManager().superSpeed(p);
+
                         return true;
                     } else if (args[0].equalsIgnoreCase("bless")) {
                         plugin.getSkillManager().bless(p, args);
+
                         return true;
                     } else if (args[0].equalsIgnoreCase("might")) {
                         plugin.getSkillManager().might(p);
+
                         return true;
                     } else if (args[0].equalsIgnoreCase("gills")) {
                         plugin.getSkillManager().gills(p);
+
                         return true;
                     } else if (args[0].equalsIgnoreCase("superJump")) {
                         plugin.getSkillManager().superJump(p);
+
                         return true;
                     } else if (args[0].equalsIgnoreCase("martyboom")) {
                         plugin.getSkillManager().martyboom(p);
+
                         return true;
                     } else if (args[0].equalsIgnoreCase("confuse")) {
                         plugin.getSkillManager().confuse(p, args);
+
                         return true;
                     }
                 } else {
@@ -172,11 +171,26 @@ public class PluginCommandExcecutor implements CommandExecutor {
                     if (args[0].equalsIgnoreCase("help")) {
                         Player player = (Player) sender;
 
-                        return skillsHelp(player, args[1]);
+                        try {
+                            int page = Integer.parseInt(args[0]);
+
+                            if (page == 1) {
+                                player.sendMessage(ChatColor.YELLOW + " ------------ " + ChatColor.WHITE + "Help: mcRP Skills (Page 1)" + ChatColor.YELLOW + " ------------");
+                                player.sendMessage(ChatColor.GOLD + "/skills superspeed" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Gives you a speed boost");
+                                player.sendMessage(ChatColor.GOLD + "/skills bless" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Heals you or a friend!");
+                                player.sendMessage(ChatColor.GOLD + "/skills might" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Increases your damage temporarily");
+                                player.sendMessage(ChatColor.GOLD + "/skills gills" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Temporarily breathe underwater");
+                                player.sendMessage(ChatColor.GOLD + "/skills superjump" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Jump higher than normal");
+                                player.sendMessage(ChatColor.GOLD + "/skills martyboom" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Detonate yourself with huge impact");
+                                player.sendMessage(ChatColor.GOLD + "/skills superpunch" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Do huge damage penetrating armour");
+                            } else {
+                                player.sendMessage(ChatColor.RED + " Invalid page number specified. There is only 1 page right now.");
+                            }
+                        } catch (NumberFormatException nfe) {
+                            player.sendMessage(ChatColor.RED + " Invalid page number specified. There is only 1 page right now.");
+                        }
                     } else {
                         sender.sendMessage(Plugin.getChatName() + " This command can only be used by players");
-
-                        return true;
                     }
                 }
             }
@@ -184,29 +198,6 @@ public class PluginCommandExcecutor implements CommandExecutor {
             sender.sendMessage("Not enough arguments. Type /skills help 1 for help");
 
             return true;
-        }
-
-        return false;
-    }
-
-    private boolean skillsHelp(Player player, String rawPage) {
-        try {
-            int page = Integer.parseInt(rawPage);
-
-            if (page == 1) {
-                player.sendMessage(ChatColor.YELLOW + " ------------ " + ChatColor.WHITE + "Help: mcRP Skills (Page 1)" + ChatColor.YELLOW + " ------------");
-                player.sendMessage(ChatColor.GOLD + "/skills superspeed" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Gives you a speed boost");
-                player.sendMessage(ChatColor.GOLD + "/skills bless" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Heals you or a friend!");
-                player.sendMessage(ChatColor.GOLD + "/skills might" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Increases your damage temporarily");
-                player.sendMessage(ChatColor.GOLD + "/skills gills" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Temporarily breathe underwater");
-                player.sendMessage(ChatColor.GOLD + "/skills superjump" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Jump higher than normal");
-                player.sendMessage(ChatColor.GOLD + "/skills martyboom" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Detonate yourself with huge impact");
-                player.sendMessage(ChatColor.GOLD + "/skills superpunch" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Do huge damage penetrating armour");
-            } else {
-                player.sendMessage(ChatColor.RED + " Invalid page number specified. There is only 1 page right now.");
-            }
-        } catch (NumberFormatException nfe) {
-            player.sendMessage(ChatColor.RED + " Invalid page number specified. There is only 1 page right now.");
         }
 
         return false;
