cfbdcfb9fa34e1ba208bdd13e35e9403f25a66ee
Keeley Hoek <escortkeel@live.com>
2012-08-31 17:54:56 +1000

    Import clean up

--- src/main/java/com/mcrp/PluginCommandExcecutor.java
+++ src/main/java/com/mcrp/PluginCommandExcecutor.java
@@ -25,14 +25,11 @@
  */
 package com.mcrp;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class PluginCommandExcecutor implements CommandExecutor {
 
