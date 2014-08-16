69ec830f2cc39d9c9b05978bdaae71a661a10777
jack lin <jacklin213@gmail.com>
2014-08-17 11:36:56 +1200

    Admin override command for /class choose

--- src/me/jacklin213/mcrp/commands/classes/CmdChoose.java
+++ src/me/jacklin213/mcrp/commands/classes/CmdChoose.java
@@ -7,6 +7,7 @@ import me.jacklin213.mcrp.classes.RPClass;
 import me.jacklin213.mcrp.commands.SubCommand;
 import me.jacklin213.mcrp.managers.CharacterManager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -27,15 +28,15 @@ public class CmdChoose extends SubCommand {
 
 	@Override
 	protected void execute(CommandSender sender, String[] args) {
-		if (args.length >= 1) {
+		if (args.length == 1) {
 			if (!(sender instanceof Player)) {
 				sender.sendMessage(CHAT_NAME + PLAYER_ONLY);
 			}
 			String className = args[0];
-			Character character = CharacterManager.getCharacter(((Player) sender));
+			Character character = CharacterManager.getCharacter((Player) sender);
 			RPClass rpClass = plugin.RPCM.getRPClass(className);
 			if (character == null) {sender.sendMessage(NULL_CHARACTER); plugin.log.warning("Error on loading Character for command /class choose"); return;}
-			if (rpClass == null) {sender.sendMessage(NO_RPCLASS); return;}
+			if (rpClass == null) {sender.sendMessage(NULL_RPCLASS); return;}
 			if (character.getRPClass().getClassType() == ClassType.NOVICE) {
 				character.setRPClass(className);
 				sender.sendMessage(YELLOW + "You have choosen the class " + GREEN + rpClass.getName());
@@ -43,8 +44,26 @@ public class CmdChoose extends SubCommand {
 				sender.sendMessage(RED + "You have already choosen the class " + GREEN + character.getRPClassName());
 				sender.sendMessage(RED + "You cant just choose another one.");
 			}
+		}
+		if (args.length == 2) {
+			if (sender.hasPermission(PERMISSIONNODE + ".admin")) {
+				if (!(sender instanceof Player)) {
+					sender.sendMessage(CHAT_NAME + PLAYER_ONLY);
+				}
+				String className = args[0];
+				Player targetPlayer = Bukkit.getPlayer(args[1]);
+				if (targetPlayer == null) {sender.sendMessage(mcRP.getChatName() + NULL_PLAYER); return;}
+				Character character = CharacterManager.getCharacter(targetPlayer);
+				RPClass rpClass = plugin.RPCM.getRPClass(className);
+				if (character == null) {sender.sendMessage(NULL_CHARACTER); plugin.log.warning("Error on loading Character for command /class choose"); return;}
+				if (rpClass == null) {sender.sendMessage(NULL_RPCLASS); return;}
+				character.setRPClass(className);
+				sender.sendMessage(YELLOW + "You have choosen the class " + GREEN + rpClass.getName() + YELLOW + " for player " + GREEN + args[1]);
+				targetPlayer.sendMessage(YELLOW + "Your class has been changed, it is now " + GREEN + rpClass.getName());
+			} else sender.sendMessage(mcRP.getChatName() + NO_PERMS);
 		} else {
 			sendHelp(sender);
 		}
 	}
+	
 }
